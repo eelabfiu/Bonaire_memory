f97df8d9a43f0a7b50ea2577b1217626241942e3
snhackerott <snhackerott@gmail.com>
2023-09-01 14:32:39 -0400

    Graphing parameters

--- 01_ExperimentalSetup.R
+++ 01_ExperimentalSetup.R
@@ -5,9 +5,38 @@
 #-------Set Up------------
 
 ####Load Packages####
-
+library(viridis)
+library(scales)
 
 ####Graphing Parameters####
+
+####Colors by Groups
+
+##Sites
+
+##Genotypes
+
+##Origins
+
+##Colors by ordered factors
+Site.colors.o<-c()
+
+Gen.colors.o<-c()
+
+Orig.colors.o<-c()
+
+
+####Plot Feature Sizes
+axis.title.sz=18
+axis.txt.sz=14
+leg.title.sz=15
+leg.txt.sz=12
+levels.sz=7
+sig.sz=5
+panel.lab.sz=25
+point.sz=4
+bar.sz=1
+cap.sz=0.3
 
 
 #-------Experimental Design------------
